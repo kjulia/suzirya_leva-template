9ff5186eec49b3f8e488c65c11a05d566c8faad4
kjulia <41785073+kjulia@users.noreply.github.com>
2018-12-14 15:01:42 +0200

    Create progress.js

---
+++ js/progress.js
@@ -0,0 +1,14 @@
+function move() {
+  var elem = document.getElementById("myBar");   
+  var width = 10;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 50) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+      elem.innerHTML = width * 1  + '%';
+    }
+  }
+}
